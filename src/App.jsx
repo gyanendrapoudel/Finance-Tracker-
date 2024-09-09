65064e2767be3d4707fbe3275aa1ce6c59d45332
Gyanendra <gyanen56@gmail.com>
2024-09-09 19:02:08 +1000

    initial router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 
 import './App.css'
 import Button from 'react-bootstrap/Button'
+import { Route, Routes } from 'react-router-dom'
 
-  import { ToastContainer, toast } from 'react-toastify'
-  import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Login from './Pages/Login'
 
 function App() {
-
 
   return (
     <>
     <h2>Hello</h2>
     <Button>Hello</Button>
     <ToastContainer/>
+     <Routes>
+      <Route  path='/' element={<Login/>}/>
+     </Routes>
     </>
   )
 }
