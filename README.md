078f6a19094ded8f638393f27b5cd2c2f2728bf5
Gyanendra <gyanen56@gmail.com>
2024-09-09 17:33:10 +1000

    initial setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project setup 
+Initial project installation react-router-dom, bootstrap, react-toastify, axios
+```
+yarn create vite ft_client
+yarn add react-router-dom
+yarn add bootstrap@5.3.3
+yarn add react-bootstrap
+yarn add react-toastify
+yarn add axios
+```
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Button from 'react-bootstrap/Button'
+
+  import { ToastContainer, toast } from 'react-toastify'
+  import 'react-toastify/dist/ReactToastify.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h2>Hello</h2>
+    <Button>Hello</Button>
+    <ToastContainer/>
+    </>
+  )
+}
+
+export default App
