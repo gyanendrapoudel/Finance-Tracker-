bba7ea088368f3d05f6c8af6b44e518ec3056a4c
Gyanendra <gyanen56@gmail.com>
2024-09-13 08:15:11 +1000

    handling on change input field

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,55 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { fetchUser } from './axios'
+import FormInput from './FormInput'
+import { Placeholder } from 'react-bootstrap'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 function BasicExample() {
+  const inputFields = [
+    { label: 'Name', type: 'text', Placeholder: 'Enter Name', name: 'name' },
+    {
+      label: 'Email',
+      type: 'email',
+      Placeholder: 'Enter Email',
+      name: 'email',
+    },
+    {
+      label: 'Password',
+      type: 'password',
+      Placeholder: 'Enter Password',
+      name: 'password',
+    },
+    {
+      label: 'Confirm Password',
+      type: 'password',
+      Placeholder: 'Confirm Password',
+      name: 'confirmPassword',
+    },
+  ]
+  const [form, setForm] = useState({})
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleOnSubmit = (e)=>{
+    e.preventDefault();
+    const{confirmPassword, ...rest}= form
+    if(confirmPassword !== rest.password) {
+      return toast.error("Password does not match!")
+    }
+  }
+
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="email" placeholder="Enter Name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
+    <Form className="p-3 m-4 shadow-lg" onSubmit={handleOnSubmit}>
+      {inputFields.map((field) => (
+        <FormInput key={field.email} {...field} onChange={handleOnChange} />
+      ))}
+      <Button variant="primary" type="submit" className="w-100">
         Submit
       </Button>
     </Form>
