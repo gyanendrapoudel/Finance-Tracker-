1e160587b092c52bafb81a084f6d4a9dc908590f
Gyanendra <gyanen56@gmail.com>
2024-09-13 07:35:53 +1000

    Form Input field using array

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,13 @@
+import Form from 'react-bootstrap/Form'
+const FormInput = ({label, ...rest}) => {
+  return (
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>{label}</Form.Label>
+      <Form.Control
+        required={true}
+        {...rest}
+      />
+    </Form.Group>
+  )
+}
+export default FormInput
