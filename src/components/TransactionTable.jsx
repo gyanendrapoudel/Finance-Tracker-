733ce5679f8297be7ff1bb05bad93f11aaa8e4f0
Gyanendra <gyanen56@gmail.com>
2024-11-23 21:52:02 +1100

    table css and adding btn for new transaction

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -3,6 +3,9 @@ import Col from 'react-bootstrap/Col'
 import { useEffect, useState } from 'react'
 import { getTransactions } from '../utils/axios'
 import { useUser } from '../context/UserContext'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { FaPlusCircle } from 'react-icons/fa'
 const TransactionTable = () => {
     const {transactions} = useUser()
     const bal = transactions.reduce((acc, t) => {
@@ -13,48 +16,66 @@ const TransactionTable = () => {
   
 
   return (
-    <Row className="p-2">
-      <h4>Transaction History</h4>
-      <table>
-        <tr>
-          <th>#</th>
-          <th>Date</th>
-          <th>Title</th>
-          <th>Out</th>
-          <th>In</th>
-        </tr>
-        {transactions.map((t, i) => {
-          return (
-            <tr key={t?._id}>
-              <td>{i + 1}</td>
-              <td>{t?.createdAt}</td>
-              <td>{t?.title}</td>
-
-              {t?.tType === 'expenses' && (
-                <>
-                  <td> -${t?.amount}</td>
-                  <td></td>
-                </>
-              )}
-              {t?.tType === 'income' && (
-                <>
-                  <td></td>
-                  <td> ${t?.amount}</td>
-                </>
-              )}
+    <>
+      <div className='d-flex justify-content-between mb-4 pt-1'>
+        <div className="">{transactions.length} transactions found!</div>
+        
+          <Form.Control type='text'/>
+          <Button>
+            <FaPlusCircle /> Add New Transaction
+          </Button>
+      </div>
+      <Row className="p-2">
+        <h4>Transaction History</h4>
+        <table className="table table-striped table-dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Out</th>
+              <th>In</th>
             </tr>
-          )
-        })}
-        <tr>
-          <th colSpan={2} className="text-center">
-            Total
-          </th>
-          <th colSpan={3} className="text-center">
-            ${bal}
-          </th>
-        </tr>
-      </table>
-    </Row>
+          </thead>
+          <tbody>
+            {transactions.map((t, i) => {
+              return (
+                <tr key={t?._id}>
+                  <td>{i + 1}</td>
+                  <td>{(t?.createdAt).slice(0, 10)}</td>
+                  <td>{t?.title}</td>
+
+                  {t?.tType === 'expenses' && (
+                    <>
+                      <td className="out"> -${t?.amount}</td>
+                      <td></td>
+                    </>
+                  )}
+                  {t?.tType === 'income' && (
+                    <>
+                      <td></td>
+                      <td className="in"> ${t?.amount}</td>
+                    </>
+                  )}
+                </tr>
+              )
+            })}
+
+            <tr>
+              <th colSpan={2} className="text-center">
+                Total
+              </th>
+              <th
+                colSpan={3}
+                className={bal > 0 ? 'text-center in ' : 'text-center out'}
+              >
+                ${bal}
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </Row>
+    </>
   )
 }
 export default TransactionTable
