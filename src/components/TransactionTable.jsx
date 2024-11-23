985dd40d096887ae4d5f02fcfe60f00d14e6f6ba
Gyanendra <gyanen56@gmail.com>
2024-11-23 19:36:55 +1100

    displaying transaction in table

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -5,23 +5,55 @@ import { getTransactions } from '../utils/axios'
 import { useUser } from '../context/UserContext'
 const TransactionTable = () => {
     const {transactions} = useUser()
+    const bal = transactions.reduce((acc, t) => {
+      let num = Number(t.amount)
+      return t?.tType === 'income' ? acc + num : acc - num
+    }, 0)
   
   
 
   return (
-    <Row className='p-2'>
+    <Row className="p-2">
       <h4>Transaction History</h4>
-      {transactions.map((t,i) => {
-        return (
-          <Row key={i} className='my-1 py-2 bg-dark'>
-            <Col>Title: {t?.title}</Col>
-            <Col>Amount: ${t?.amount} </Col>
-            <Col>Type: {t?.tType}</Col>
-          </Row>
-        )
-      })}
+      <table>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Title</th>
+          <th>Out</th>
+          <th>In</th>
+        </tr>
+        {transactions.map((t, i) => {
+          return (
+            <tr key={t?._id}>
+              <td>{i + 1}</td>
+              <td>{t?.createdAt}</td>
+              <td>{t?.title}</td>
 
-      
+              {t?.tType === 'expenses' && (
+                <>
+                  <td> -${t?.amount}</td>
+                  <td></td>
+                </>
+              )}
+              {t?.tType === 'income' && (
+                <>
+                  <td></td>
+                  <td> ${t?.amount}</td>
+                </>
+              )}
+            </tr>
+          )
+        })}
+        <tr>
+          <th colSpan={2} className="text-center">
+            Total
+          </th>
+          <th colSpan={3} className="text-center">
+            ${bal}
+          </th>
+        </tr>
+      </table>
     </Row>
   )
 }
